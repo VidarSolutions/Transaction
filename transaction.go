53450b56aae0a65d8ee974875162d2886c258978
Jeff <jeff@fgeth.com>
2023-04-28 07:58:14 -0500

    fix imports

--- transaction.go
+++ transaction.go
@@ -13,18 +13,18 @@ type Tx struct{
 	TID			uint64							//Transaction Id  first 4 digits are the year
 	From		uint64							//BundleID as PublicKey where funds are coming from
 	To			uint64							//BundleID as PublicKey where funds are going to
-	Sender		ed25529.PublicKey				//Sender and signer of transaction public key that is a one time use just for this trasnaction
+	Sender		ed25519.PublicKey				//Sender and signer of transaction public key that is a one time use just for this trasnaction
 	Password	string							//Password of current owner.  Publicky an dpassword are hashed and compared to the signed hash	
 	Value		uint64							//Value of this transaction
 	LTID		uint64							//Transaction Id from the last transaction sent from this bundle
-	Phash		appData.bytes32					//Hash from last transaction
-	Hash		appData.bytes32					//Hash for next password and new publickey for sending bundle that prove ownership of bundle
-	Sig			appData.bytes64					//Signature of new keys that own the bundle sending the trasaction
+	Phash		bytes32							//Hash from last transaction
+	Hash		bytes32							//Hash for next password and new publickey for sending bundle that prove ownership of bundle
+	Sig			bytes64							//Signature of new keys that own the bundle sending the trasaction
 	SID			uint64							//Number of transactions sent by this sender not including this transaction  
 	RID			uint64							//Number of transactions receieved by this receiever not including this transaction  
 	Approved	bool							//If transaction has been approved
-	Approver	ed25529.PublicKey				//PublicKey of Approver  Must be from an authorized approver for this bundle
-	Signed		appData.bytes64					//Hash of transaction signed by approver
+	Approver	ed25519.PublicKey				//PublicKey of Approver  Must be from an authorized approver for this bundle
+	Signed		bytes64							//Hash of transaction signed by approver
 
 
 }
@@ -32,8 +32,8 @@ type Tx struct{
 var(
 
 //setup map of transactions
-	Transactions		map[uint64]Transaction			//Known Transactions
-	notValid  			map[uint64][]transaction		//Map of potential transactions ID to node IDs indicating a bad transaction was approved. We do not track valid transaction in a map that is track in each bundle, but this is a way to track and vote a bad transaction be reversed and removed.
+	Transactions		map[uint64]Tx					//Known Transactions
+	notValid  			map[uint64][]txID				//Map of potential transactions ID to node IDs indicating a bad transaction was approved. We do not track valid transaction in a map that is track in each bundle, but this is a way to track and vote a bad transaction be reversed and removed.
 	authorized  		map[uint64][]uint64				//Map of bundle IDs to a list of Authorized Node IDs
 
 )
