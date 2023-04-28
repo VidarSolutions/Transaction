615b0a31148776e46ada0036c179ca801125de4a
Jeff <jeff@fgeth.com>
2023-04-28 08:00:27 -0500

    Fix case

--- transaction.go
+++ transaction.go
@@ -17,14 +17,14 @@ type Tx struct{
 	Password	string							//Password of current owner.  Publicky an dpassword are hashed and compared to the signed hash	
 	Value		uint64							//Value of this transaction
 	LTID		uint64							//Transaction Id from the last transaction sent from this bundle
-	Phash		bytes32							//Hash from last transaction
-	Hash		bytes32							//Hash for next password and new publickey for sending bundle that prove ownership of bundle
-	Sig			bytes64							//Signature of new keys that own the bundle sending the trasaction
+	Phash		Bytes32							//Hash from last transaction
+	Hash		Bytes32							//Hash for next password and new publickey for sending bundle that prove ownership of bundle
+	Sig			Bytes64							//Signature of new keys that own the bundle sending the trasaction
 	SID			uint64							//Number of transactions sent by this sender not including this transaction  
 	RID			uint64							//Number of transactions receieved by this receiever not including this transaction  
 	Approved	bool							//If transaction has been approved
 	Approver	ed25519.PublicKey				//PublicKey of Approver  Must be from an authorized approver for this bundle
-	Signed		bytes64							//Hash of transaction signed by approver
+	Signed		Bytes64							//Hash of transaction signed by approver
 
 
 }
@@ -33,7 +33,7 @@ var(
 
 //setup map of transactions
 	Transactions		map[uint64]Tx					//Known Transactions
-	notValid  			map[uint64][]txID				//Map of potential transactions ID to node IDs indicating a bad transaction was approved. We do not track valid transaction in a map that is track in each bundle, but this is a way to track and vote a bad transaction be reversed and removed.
+	notValid  			map[uint64][]uint64				//Map of potential transactions ID to node IDs indicating a bad transaction was approved. We do not track valid transaction in a map that is track in each bundle, but this is a way to track and vote a bad transaction be reversed and removed.
 	authorized  		map[uint64][]uint64				//Map of bundle IDs to a list of Authorized Node IDs
 
 )
